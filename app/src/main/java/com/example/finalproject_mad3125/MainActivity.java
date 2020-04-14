ae52f7f73bc1f7768702c7ee2fad743c974ab54d
roopvirk6 <virkikroop13@gmail.com>
2020-04-13 19:32:46 -0600

    [IKROOP] declared edittext and button in login activity

--- app/src/main/java/com/example/finalproject_mad3125/MainActivity.java
+++ app/src/main/java/com/example/finalproject_mad3125/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.finalproject_mad3125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView2;
+    TextInputEditText editText1;
+    TextInputEditText edtPassword;
+    SwitchMaterial rememberMe;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView2 = (ImageView)findViewById(R.id.imageView2);
+        editText1 = (TextInputEditText)findViewById(R.id.editText1);
+        edtPassword = (TextInputEditText)findViewById(R.id.edtPassword);
+        rememberMe = (SwitchMaterial)findViewById(R.id.rememberMe);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
     }
 }
