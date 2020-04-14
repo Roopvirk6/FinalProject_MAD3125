ecf013db4a0d48e0f42d5a05c78f8d1bc3d0d8e8
roopvirk6 <virkikroop13@gmail.com>
2020-04-13 18:13:17 -0600

    [IKROOP] splash activity created

--- app/src/main/java/com/example/finalproject_mad3125/SplashActivity.java
+++ app/src/main/java/com/example/finalproject_mad3125/SplashActivity.java
@@ -1,10 +1,15 @@
 package com.example.finalproject_mad3125;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -18,6 +23,24 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         textView1 = (TextView)findViewById(R.id.textView1);
         imageView1 = (ImageView)findViewById(R.id.imageView1);
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"C0774174 IKROOP VIRK", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        },duration);
 
     }
 }
