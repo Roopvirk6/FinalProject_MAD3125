9216bef4368da621a33d7da8ba7eda886dbeb621
roopvirk6 <virkikroop13@gmail.com>
2020-04-13 18:05:56 -0600

    [IKROOP] splash activity created

--- app/src/main/java/com/example/finalproject_mad3125/SplashActivity.java
+++ app/src/main/java/com/example/finalproject_mad3125/SplashActivity.java
@@ -3,12 +3,20 @@ package com.example.finalproject_mad3125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    TextView textView1;
+    ImageView imageView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        textView1 = (TextView)findViewById(R.id.textView1);
+        imageView1 = (ImageView)findViewById(R.id.imageView1);
+        
     }
 }
