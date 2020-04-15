c6587379fe53757890b3e0d9e23eace7f1b0ead6
roopvirk6 <virkikroop13@gmail.com>
2020-04-15 15:16:20 -0600

    [IKROOP] customer class implements parceable

--- app/src/main/java/com/example/finalproject_mad3125/Customer.java
+++ app/src/main/java/com/example/finalproject_mad3125/Customer.java
@@ -1,9 +1,28 @@
 package com.example.finalproject_mad3125;
 
+import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Customer implements Parcelable
 {
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Customer createFromParcel(Parcel in) {
+            return new Customer(in);
+        }
+
+        public Customer[] newArray(int size) {
+            return new Customer[size];
+        }
+    };
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
 }
