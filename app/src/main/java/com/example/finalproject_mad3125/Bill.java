6cb4ebdffd86155a510811a63c2f6b483202cb10
roopvirk6 <virkikroop13@gmail.com>
2020-04-15 16:39:32 -0600

    [IKROOP] bill class implements serialisable

--- app/src/main/java/com/example/finalproject_mad3125/Bill.java
+++ app/src/main/java/com/example/finalproject_mad3125/Bill.java
@@ -4,5 +4,56 @@ import java.io.Serializable;
 
 public abstract class Bill implements Serializable
 {
+    private int billID;
+    private String billDate;
+    private String billType;
+    private double billAmount;
+
+    public Bill()
+    {
+
+    }
+
+    public Bill(int billID, String billDate, String billType,double billAmount)
+    {
+        this.billID = billID;
+        this.billDate = billDate;
+        this.billType = billType;
+        this.billAmount = billAmount;
+    }
+
+    public int getBillID() {
+        return billID;
+    }
+
+    public void setBillID(int billID) {
+        this.billID = billID;
+    }
+
+    public String getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(String billDate) {
+        this.billDate = billDate;
+    }
+
+    public String getBillType() {
+        return billType;
+    }
+
+    public void setBillType(String billType) {
+        this.billType = billType;
+    }
+
+    public double getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(double billAmount) {
+        this.billAmount = billAmount;
+    }
+
     
+
 }
