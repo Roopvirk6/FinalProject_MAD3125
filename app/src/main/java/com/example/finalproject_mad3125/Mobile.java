80847845eaf543f8340219840b65416fd54425ca
roopvirk6 <virkikroop13@gmail.com>
2020-04-15 18:52:06 -0600

    [IKROOP] MOBILE class variables declared

--- app/src/main/java/com/example/finalproject_mad3125/Mobile.java
+++ app/src/main/java/com/example/finalproject_mad3125/Mobile.java
@@ -4,5 +4,13 @@ import java.io.Serializable;
 
 public class Mobile extends Bill implements Serializable
 {
+    private String manufacturerName;
+    private String planName;
+    private int mobileNumber;
+    private int internetGBUesd;
+    private int minutes;
+
+
     
+
 }
