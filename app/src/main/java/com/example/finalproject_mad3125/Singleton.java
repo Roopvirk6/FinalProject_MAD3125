8335578f0d33b6510217aa216e4a22c1bbe9b6e1
roopvirk6 <virkikroop13@gmail.com>
2020-04-15 21:06:54 -0600

    [IKROOP] function declared

--- app/src/main/java/com/example/finalproject_mad3125/Singleton.java
+++ app/src/main/java/com/example/finalproject_mad3125/Singleton.java
@@ -7,5 +7,8 @@ public class Singleton
     private static Singleton myObj;
 
     ArrayList<Customer> arrayListCustomer = new ArrayList<>();
-    
+    private Singleton()
+    {
+        
+    }
 }
