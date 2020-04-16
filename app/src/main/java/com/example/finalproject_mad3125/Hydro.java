3574172a5e1542e0ca5406d80be07acebfc4763a
roopvirk6 <virkikroop13@gmail.com>
2020-04-15 19:37:39 -0600

    [IKROOP] Hydro class updated

--- app/src/main/java/com/example/finalproject_mad3125/Hydro.java
+++ app/src/main/java/com/example/finalproject_mad3125/Hydro.java
@@ -4,5 +4,21 @@ import java.io.Serializable;
 
 public class Hydro extends Bill implements Serializable
 {
+    private String agencyName;
+    private String unitConsumed;
+
+    public Hydro()
+    {
+        super();
+    }
+
+    public Hydro(int billID, String billDate, String billType, double billAmount, String agencyName, String unitConsumed)
+    {
+        super(billID,billDate,"Hydro",billAmount);
+        this.agencyName = agencyName;
+        this.unitConsumed = unitConsumed;
+    }
+
     
+
 }
