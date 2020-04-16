63cdcbe2de05109adfa50dc35613fc6bf9108ca6
roopvirk6 <virkikroop13@gmail.com>
2020-04-15 20:36:42 -0600

    [IKROOP] OBJECT DECLARED

--- app/src/main/java/com/example/finalproject_mad3125/Singleton.java
+++ app/src/main/java/com/example/finalproject_mad3125/Singleton.java
@@ -1,6 +1,11 @@
 package com.example.finalproject_mad3125;
 
+import java.util.ArrayList;
+
 public class Singleton
 {
+    private static Singleton myObj;
+
+    ArrayList<Customer> arrayListCustomer = new ArrayList<>();
     
 }
